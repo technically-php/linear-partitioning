3789eeebb25597bcb5665748d65ea532d1375f60
e1himself <ivan.voskoboinyk@gmail.com>
2017-02-20 19:50:20 +0200

    Rewrite global function to a namespaced static class
    Static class is better because it leverages autoloading

--- test.php
+++ test.php
@@ -1,22 +1,24 @@
 <?php
 
-require 'src/functions.php';
+use TechnicallyPhp\LinearPartitioning;
+
+require 'src/LinearPartitioning.php';
 
 function validate(array $expected, array $actual) {
     assert($expected == $actual, json_encode($expected) . ' !== ' . json_encode($actual));
 }
 
 validate(
-    partition([1,2,3,4,5,6,7,8,9], 3),
+    LinearPartitioning::partition([1,2,3,4,5,6,7,8,9], 3),
     [[1,2,3,4,5], [6,7], [8,9]]
 );
 
 validate(
-    partition([1,1,1,1,1,1,1,1,1], 3),
+    LinearPartitioning::partition([1,1,1,1,1,1,1,1,1], 3),
     [[1,1,1], [1,1,1], [1,1,1]]
 );
 
 validate(
-    partition([1,1,1,1,1,1,1,1,1], 1),
+    LinearPartitioning::partition([1,1,1,1,1,1,1,1,1], 1),
     [[1,1,1,1,1,1,1,1,1]]
 );
