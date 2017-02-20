b3d515fed498d4633fd56daf4fa9c8a3bbbac0e0
e1himself <ivan.voskoboinyk@gmail.com>
2017-02-20 19:29:43 +0200

    Improve output on failed test cases

--- test.php
+++ test.php
@@ -2,14 +2,21 @@
 
 require 'src/functions.php';
 
-assert(
-    partition([1,2,3,4,5,6,7,8,9], 3) == [[1,2,3,4,5], [6,7], [8,9]]
+function validate(array $expected, array $actual) {
+    assert($expected == $actual, json_encode($expected) . ' !== ' . json_encode($actual));
+}
+
+validate(
+    partition([1,2,3,4,5,6,7,8,9], 3),
+    [[1,2,3,4,5], [6,7], [8,9]]
 );
 
-assert(
-    partition([1,1,1,1,1,1,1,1,1], 3) == [[1,1,1], [1,1,1], [1,1,1]]
+validate(
+    partition([1,1,1,1,1,1,1,1,1], 3),
+    [[1,1,1], [1,1,1], [1,1,1]]
 );
 
-assert(
-    partition([1,1,1,1,1,1,1,1,1], 1) == [[1,1,1,1,1,1,1,1,1]]
+validate(
+    partition([1,1,1,1,1,1,1,1,1], 1),
+    [[1,1,1,1,1,1,1,1,1]]
 );
